22b6621040a41427ea2c6b06fad5fe9dcf38db19
molnar <molnar@inf.u-szeged.hu>
2022-02-05 18:06:49 +0100

    new feature 5

---
+++ feladat.js
@@ -0,0 +1,21 @@
+let codes = {"49": "Germany", "41": "Switzerland", "44": "Great Britain ", "1": "USA"};
+for (let code in codes) {
+    setInterval(1000)
+    document.getElementById("demo").innerHTML = code;
+}
+console.log('sodjfpos')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
